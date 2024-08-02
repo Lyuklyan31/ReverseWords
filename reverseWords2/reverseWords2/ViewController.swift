45f149ec80b2e40adbda43598f62e6a6ed23e567
oleg <lyuclyan.oleg14@gmail.com>
2024-08-02 11:09:06 +0300

    First commit + gitignore

---
+++ reverseWords2/reverseWords2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reverseWords2
+//
+//  Created by admin on 02.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
