f7d3d75ff5c086a0e14e052d6d99e853898badd9
oleg <lyuclyan.oleg14@gmail.com>
2024-08-03 04:22:33 +0300

    bug fixed

--- reverseWords2/reverseWords2/ReverseWordsService.swift
+++ reverseWords2/reverseWords2/ReverseWordsService.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 protocol ReverseWordsServiceProtocol {
-    func reverseWords(in text: String, ignoring ignoreWords: Set<String>, reverseDigits: Bool, reverseSpecialCharacters: Bool, isCustomMode: Bool) -> String
+    func reverseWords(in text: String, ignoring ignoreWords: Set<String>, reverseDigits: Bool, reverseSpecialCharacters: Bool, isCustomMode: Bool, ignoreCharacters: Set<Character>) -> String
 }
 
 class ReverseWordsService: ReverseWordsServiceProtocol {
-    func reverseWords(in text: String, ignoring ignoreWords: Set<String>, reverseDigits: Bool, reverseSpecialCharacters: Bool, isCustomMode: Bool) -> String {
+    func reverseWords(in text: String, ignoring ignoreWords: Set<String>, reverseDigits: Bool, reverseSpecialCharacters: Bool, isCustomMode: Bool, ignoreCharacters: Set<Character>) -> String {
         let words = text.components(separatedBy: " ")
         var reversedWords = [String]()
         
@@ -20,19 +20,19 @@ class ReverseWordsService: ReverseWordsServiceProtocol {
             if ignoreWords.contains(word) {
                 reversedWords.append(word)
             } else {
-                let reversedWord = reverseCharacters(in: word, reverseDigits: reverseDigits, reverseSpecialCharacters: reverseSpecialCharacters, isCustomMode: isCustomMode)
+                let reversedWord = reverseCharacters(in: word, reverseDigits: reverseDigits, reverseSpecialCharacters: reverseSpecialCharacters, isCustomMode: isCustomMode, ignoreCharacters: ignoreCharacters)
                 reversedWords.append(reversedWord)
             }
         }
         return reversedWords.joined(separator: " ")
     }
     
-    private func reverseCharacters(in word: String, reverseDigits: Bool, reverseSpecialCharacters: Bool, isCustomMode: Bool) -> String {
+    private func reverseCharacters(in word: String, reverseDigits: Bool, reverseSpecialCharacters: Bool, isCustomMode: Bool, ignoreCharacters: Set<Character>) -> String {
         var charactersToReverse = [Character]()
         var result = Array(word)
     
         for char in word {
-            if char.isLetter && !(isCustomMode && (char == "x" || char == "l")) {
+            if char.isLetter && !ignoreCharacters.contains(char) {
                 charactersToReverse.append(char)
             } else if reverseDigits && char.isNumber {
                 charactersToReverse.append(char)
@@ -45,7 +45,7 @@ class ReverseWordsService: ReverseWordsServiceProtocol {
         
         var index = 0
         for i in 0..<result.count {
-            if (result[i].isLetter && !(isCustomMode && (result[i] == "x" || result[i] == "l"))) ||
+            if (result[i].isLetter && !ignoreCharacters.contains(result[i])) ||
                (reverseDigits && result[i].isNumber) ||
                (reverseSpecialCharacters && containsSpecialCharacter(result[i])) {
                 result[i] = charactersToReverse[index]
@@ -61,4 +61,3 @@ class ReverseWordsService: ReverseWordsServiceProtocol {
         return char.unicodeScalars.allSatisfy { specialCharacters.contains($0) }
     }
 }
-
